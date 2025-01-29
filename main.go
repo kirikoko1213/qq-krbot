ea8da1606684c4b79910cadff71783f487ba36c0
kiririx <kiririx1213@gmail.com>
2025-01-29 19:09:33 +0800

    refactor(main): Remove static file serving and frontend routing
    - Removed embedded static file serving for manage-board
    - Deleted NoRoute handler for serving index.html
    - Simplified main function routing configuration

--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"embed"
-	"github.com/gin-gonic/gin"
-	"net/http"
 	"qq-krbot/api"
 	"qq-krbot/env"
 	"qq-krbot/work"
-)
 
-//go:embed manage-board/dist/*
-var staticFiles embed.FS
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	work.Boardcast()
 	r := gin.Default()
 	r.Use(gin.Recovery())
 	api.RegisterRouter(r)
-
-	// 提供静态文件服务
-	// 利用 gin 的 StaticFS 方法提供静态文件
-	staticFS := http.FS(staticFiles)
-	r.StaticFS("/static", staticFS)
-
-	// 处理所有其他请求，重定向到index.html
-	r.NoRoute(func(c *gin.Context) {
-		c.FileFromFS("manage-board/dist/index.html", staticFS)
-	})
-
 	_ = r.Run(":" + env.Get("serve.port"))
 }
